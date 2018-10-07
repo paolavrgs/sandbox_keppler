29b96960a40c8a39b2b61bb7b4fd54ab15e337e4
Paola Vargas <vrgspaola68@gmail.com>
2018-10-07 19:37:27 -0400

    Products demo 03, carousel and tabs

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -24,6 +24,24 @@ $('.multiple-items').slick({
   ]
 });
 
+$('.products-slick').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+
+// TABS CLICK ADD CLASS ACTIVE
+$("#myTab .products-card").on('click' , function(){
+  $(".products-slick .products-card").removeClass('active');
+});
+
+// TABS CHANGE WHEN SLICK CHANGE
+$('.products-slick').on('afterChange', function(event, slick, currentSlide, nextSlide){
+  $(".products-slick .products-card").removeClass('active');
+
+  $(".products-slick .products-card[data-slick-current="+ currentSlide +"]").tab('show')
+});
+
 jQuery(function(){
   createSticky(jQuery(".sticky-custom"));
 });
