e2a0117f7399520be40a1794071f7e004eb60f46
Paola Vargas <vrgspaola68@gmail.com>
2018-10-08 22:13:23 -0400

    Details, footer, contact form

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.element', {
-  strings: ["Galería", "Blog", "Catálogo"],
+  strings: ['Galería', 'Blog', 'Catálogo'],
   typeSpeed: 100,
   showCursor: false,
   loop: true,
@@ -28,6 +28,10 @@ $('.products-slick').slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: 0,
+  arrows: false,
+  dots: true,
   responsive: [
     {
       breakpoint: 480,
@@ -47,32 +51,36 @@ $('.products-slick').slick({
 });
 
 // TABS CLICK ADD CLASS ACTIVE
-$("#myTab .products-card").on('click' , function(){
-  $(".products-slick .products-card").removeClass('active');
+$('#myTab .products-card').on('click' , function(){
+  $('.products-slick .products-card').removeClass('active');
+  // TABS CLICK ADD CLASS ACTIVE ON DOT RELATED
+  var id = $(this).attr('data-id')
+  $('.slick-dots').find('li').removeClass('slick-active')
+  $(`#slick-slide-control1${id}`).parent().addClass('slick-active')
 });
 
 // TABS CHANGE WHEN SLICK CHANGE
-$('.products-slick').on('afterChange', function(event, slick, currentSlide, nextSlide){
-  $(".products-slick .products-card").removeClass('active');
+$('.products-slick').on('afterChange', function(event, slick, currentSlide, nextSlide) {
+  $('.products-slick .products-card').removeClass('active');
 
-  $(".products-slick .products-card[data-slick-current="+ currentSlide +"]").tab('show')
+  $(`.products-slick .products-card[data-slick-current=${currentSlide}]`).tab('show')
 });
 
 jQuery(function(){
-  createSticky(jQuery(".sticky-custom"));
+  createSticky(jQuery('.sticky-custom'));
 });
 
 function createSticky(sticky) {
-  if (typeof sticky != "undefined") {
+  if (typeof sticky != 'undefined') {
 
     var pos = sticky.offset().top + 150,
         win = jQuery(window);
 
-    win.on("scroll", function() {
+    win.on('scroll', function() {
         if( win.scrollTop() > pos ) {
-            sticky.addClass("stickyhead");
+            sticky.addClass('stickyhead');
         } else {
-            sticky.removeClass("stickyhead");
+            sticky.removeClass('stickyhead');
         }           
     });         
   }
@@ -86,11 +94,11 @@ $(document).on('scroll', function () {
   });
   $(window).scroll(function () {
     $.each(array, function (x, v) {
-      if ($(window).scrollTop() >= ($("#" + v).position().top - 100) && $(window).scrollTop() <= ($("#" + v).position().top - 100) + $("#" + v).outerHeight(true)) {
-        $('.nav-link[href=\"#' + v + '\"]').addClass('active');
+      if ($(window).scrollTop() >= ($('#' + v).position().top - 100) && $(window).scrollTop() <= ($('#' + v).position().top - 100) + $('#' + v).outerHeight(true)) {
+        $('.nav-link[href=\'#' + v + '\']').addClass('active');
       }
       else {
-        $('.nav-link[href=\"#' + v + '\"]').removeClass('active');
+        $('.nav-link[href=\'#' + v + '\']').removeClass('active');
       }
     })
   })
